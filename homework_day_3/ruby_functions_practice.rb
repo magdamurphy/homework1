a7525372759217e9dd8ae5b66efda50d149b47a0
magdam <magdalena2019@gmail.com>
2016-12-06 19:14:50 +0000

    homework

---
+++ homework_day_3/ruby_functions_practice.rb
@@ -0,0 +1,35 @@
+def add(a,b)
+  sum = a+b
+return sum
+end
+
+def subtract(a,b)
+minus = a-b
+return minus
+end
+
+
+def multiply(a,b)
+multiply = a*b
+  return multiply
+end
+
+def divide(a,b)
+  divide = a/b
+    return divide
+end
+
+def length_of_string(a)
+variable = a.length()
+return variable
+end
+
+def join_string(a,b)
+  variable = "#{a}#{b}"
+  return variable
+end
+
+def add_string_as_number(a,b)
+variable = a.to_i + b.to_i
+return variable
+end
